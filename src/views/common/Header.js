a3f9643b3d73c4942d636fc9a8df5a3f02007487
chiragghevariya <gcb1196@gmail.com>
2020-05-07 17:16:40 +0530

    added Link insted of a tag to prevent load page

--- src/views/common/Header.js
+++ src/views/common/Header.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
      
     <header id="header" className="fixed-top header-inner-pages">
     <div className="container d-flex align-items-center">
-      <h1 className="logo mr-auto"><a href="/">Tempo</a></h1>
+      <h1 className="logo mr-auto"><Link to="/">Tempo</Link></h1>
         <nav className="nav-menu d-none d-lg-block">
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
-          <li className="active"><a href="/blog">Blog</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="#about">About</Link></li>
+          <li><Link to="#services">Services</Link></li>
+          <li><Link to="#portfolio">Portfolio</Link></li>
+          <li><Link to="#team">Team</Link></li>
+          <li className="active"><Link to="/blog">Blog</Link></li>
+          <li><Link to="#contact">Contact</Link></li>
 
         </ul>
       </nav>
